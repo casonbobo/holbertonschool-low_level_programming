9e6ee787d36edde2de26630ccf5ef3662dffcf85
casonbobo <casonsbobo@gmail.com>
2023-01-25 10:20:34 -0800

    push

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+* main:POOPOO
+* Return:PEEPEE
+*
+*/
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+char prog[] = "with proper grammar, but the outcome is a piece of art,";
+prinf(prog);
+return (0);
+}
