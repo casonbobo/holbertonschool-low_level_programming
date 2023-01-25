7df74251e6954bffe8bd2f7b4f822d848bc67874
casonbobo <casonsbobo@gmail.com>
2023-01-25 13:23:28 -0800

    push

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+/**
+*Main - HEHE
+*Return: HAHA
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n%1 >= 1)
+{
+printf("%n is positive");
+else if (n%1 = 0)
+printf("%n is zero");
+else if (n%1 < 0)
+printf("%n is negative");
+}
+return (0);
+}					
