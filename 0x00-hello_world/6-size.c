4ccd95feea4a8285b598c5215610f1a91389d60d
casonbobo <casonsbobo@gmail.com>
2023-01-25 12:00:10 -0800

    push

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+/**
+* main - POOPOO
+* Return:PEEPEE
+*
+*/
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+printf("Size of a char: %i byte(s)\n", sizeof(char));
+printf("Size of a int: %i byte(s)\n", sizeof(int));
+printf("Size of a long int: %i byte(s)\n", sizeof(long));
+printf("Size of a long long int: %i byte(s)\n", sizeof(long long int)); 
+printf("Size of a float: %i byte(s)\n", sizeof(float));
+}
