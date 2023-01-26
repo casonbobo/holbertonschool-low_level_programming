4835d44ee3c55ee59a77e7aa6a04f3ef406f12c1
casonbobo <casonsbobo@gmail.com>
2023-01-26 12:09:10 -0800

    poiuyt

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+/**
+*main - Time to run
+*Return: Or time to turn
+*/
+#include <stdio.h>
+
+int main(void)
+{
+int c;
+for (c = 0; c <= 9; c++)
+{
+putchar('0' + c);
+putchar(',');
+putchar(' ');
+
+}
+putchar ('\n');
+return (0);
+}
