89d1f2a7a7db958205b16a20b4bfe829e26a6c2e
casonbobo <casonsbobo@gmail.com>
2023-01-26 11:31:36 -0800

    push

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+/**
+*main - Time to run
+*Return: Or time to turn
+*/
+#include <stdio.h>
+
+int main(void)
+{
+int c;
+for (c = 0; c <= 9; c++)
+{
+putchar(c);
+}
+putchar ('\n');
+return (0);
+}
