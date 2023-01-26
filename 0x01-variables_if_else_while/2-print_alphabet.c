0c3d09dcac94c793ab11607bfdcd2529c397b4a8
casonbobo <casonsbobo@gmail.com>
2023-01-25 21:28:59 -0800

    abc2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,12 @@
 
 int main(void)
 {
-
-char ch = "a";
-
-for (ch; ch <= "z"; ch++)
-puthar(ch);
-
+str arr1[] = {a, b, c, d, e, f, g, h, \\
+i, j, k, l, m, n, o, p, q, \\
+r, s, t, u, v, w, x, y, z};
+for (int i = 0; i < 25; i++)
+{
+putchar("%d", arr[i]);
+}
 return (0);
 }
