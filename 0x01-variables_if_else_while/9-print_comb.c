4746dfde342800662992cf6f09542f46a99c1c8f
casonbobo <casonsbobo@gmail.com>
2023-01-26 12:14:28 -0800

    poiuyt

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,16 @@ int main(void)
 int c;
 for (c = 0; c <= 9; c++)
 {
+if ( c != 9)
+{
 putchar('0' + c);
 putchar(',');
 putchar(' ');
-
 }
-putchar ('\n');
+else
+{
+putchar('0' + c);
+}
+}
 return (0);
 }
