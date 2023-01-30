e9a2755806b908eb0269f4af6e8eeb4ce3604cf0
casonbobo <casonsbobo@gmail.com>
2023-01-30 14:43:45 -0800

    poiuy

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+/**
+*main - thats really weird
+*
+*Return: git these cheeks
+*/
+#include "main.h"
+
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+	return (sum);
+
+
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,4 +14,5 @@ int print_sign(int n);
 int _abs(int n);
 int print_last_digit(int r);
 void jack_bauer(void);
+void times_table(void);
 #endif
