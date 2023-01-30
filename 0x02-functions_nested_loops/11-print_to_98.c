226788569425044472d2fdda4e918181590c227f
casonbobo <casonsbobo@gmail.com>
2023-01-30 15:25:12 -0800

    poiuyt

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,14 +9,16 @@ void print_to_98(int n)
 {
 if (n >= 98)
 {
-	while (n > 98)
+	while (n > 98){
 		printf("%d, ", n--);
 		printf("%d\n", n);
+	}
 }
 else
 {
-	while (n < 98)
+	while (n < 98){
 		printf("%d, ", n++);
 		printf("%d\n", n);
+	}
 }
 }
