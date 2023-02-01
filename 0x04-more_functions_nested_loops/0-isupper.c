9f4ccde2334bf64a7e031677b971a32a09cfc490
casonbobo <casonsbobo@gmail.com>
2023-02-01 11:54:37 -0800

    first push of the day

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+/**
+*_isupper - Back at it
+*
+*Return: 1 or 0
+*/
+
+#include "main.h"
+
+int _isupper(int c)
+{
+if (c == '65 .. 90')
+	Return(1);
+else
+	Return(0);
+
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+/**
+*main - I forgot what went here
+*Return: Soemthing for sure
+*/
+
+#include <stdio.h>
+#ifndef _PUTCHAR
+#define _PUTCHAR 
+
+int _putchar(char c);
+int _issupper(int c);
+#endif
