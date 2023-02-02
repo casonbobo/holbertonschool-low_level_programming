b0d9efbbf88299b2f355b5c4e0c288701b3018eb
casonbobo <casonsbobo@gmail.com>
2023-02-02 11:46:44 -0800

    holy carp

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,13 +15,12 @@ for (c = 0; c < 10; c++)
 {
 	for (i = 0; i <= 14; i++)
 	{
-			if (i > 9)
-			{
-				_putchar((i / 10) + '0');
-			}
-			_putchar((i % 10) + '0');
+		if (i > 9)
+		{
+		_putchar((i / 10) + '0');
+		}
+	_putchar((i % 10) + '0');
 	}
-	_putchar('\n');
+_putchar('\n');
 }
-return;
 }
