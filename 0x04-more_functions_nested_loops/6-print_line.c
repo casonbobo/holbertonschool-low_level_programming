c32e2af91c1790ef17a1a05245ac1e515453460e
casonbobo <casonsbobo@gmail.com>
2023-02-02 12:05:16 -0800

    14th i think

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+/**
+*print_line - draw
+*@n: an incrementor
+*Return: nuthin
+*/
+
+#include "main.h"
+
+void print_line(int n)
+{
+char line = '_';
+int i;
+
+	for (i = 0; i == n; i++)
+	{
+		_putchar(line);
+	}
+}
--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -6,6 +6,9 @@
 
 int main(void)
 {
-	    more_numbers();
-	        return (0);
+	    print_line(0);
+	        print_line(2);
+		    print_line(10);
+		        print_line(-4);
+			    return (0);
 }
