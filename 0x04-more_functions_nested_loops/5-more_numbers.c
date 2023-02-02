71b95e5168e37c6d82df29bca85d666912a83b9d
casonbobo <casonsbobo@gmail.com>
2023-02-02 11:12:07 -0800

    plzzzz

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,13 +14,13 @@ int c;
 for (c = 0; c < 10; c++)
 {
 	for (i = 0; i <= 14; i++)
-		{
+	{
 			if (i > 9)
 			{
-			_putchar((i / 10) + '0');
+				_putchar((i / 10) + '0');
 			}
-		_putchar((i % 10) + '0');
-		}
+			_putchar((i % 10) + '0');
+	}
 	_putchar('\n');
 }
 return;
