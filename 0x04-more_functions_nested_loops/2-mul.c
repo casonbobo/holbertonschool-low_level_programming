0628e5295721b89529003d8f0fdbe1d8398519b2
casonbobo <casonsbobo@gmail.com>
2023-02-02 09:15:37 -0800

    whoopsies

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,15 +1,16 @@
 /**
-*mul - makes the product 
+*mul - makes the product
 *@a: is a number secretly
 *@b wants to be a letter again
-*Retrun: 0
+*Return: 0
 */
+#include "main.h"
 
 int mul(int a, int b)
 {
 int product;
 
-a * b = product;
+a *b = product;
 
 return (0);
 }
