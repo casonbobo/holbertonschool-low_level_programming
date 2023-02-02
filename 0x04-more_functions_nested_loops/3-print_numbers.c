9ea7b84f3c6d5fc1f35368b9e248bb713a8a579c
casonbobo <casonsbobo@gmail.com>
2023-02-02 10:21:14 -0800

    7th

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+/**
+*print_numbers - prints... numbers...
+*Return: 0
+*/
+
+#include "main.h"
+
+void print_numbers(void)
+{
+
+int i = 0;
+for (; i <= 0; i++)
+	{
+	_putchar(i);
+	}
+_putchar('\n');
+return;
+}
