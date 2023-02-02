46fcaeb07fc5d66381cde93d84c478cf6fcdc1e6
casonbobo <casonsbobo@gmail.com>
2023-02-02 11:05:54 -0800

    im tired of this

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,14 +11,14 @@ void more_numbers(void)
 int i;
 int c;
 
-for (c = 0; c <= 10; c++)
+for (c = 0; c < 10; c++)
 {
 	for (i = 0; i <= 14; i++)
 		{
-		_putchar(i + '0');
+		_putchar((i / 10) + '0');
+		_putchar((i % 10) + '0');
 		}
 	_putchar('\n');
 }
-_putchar('\n');
 return;
 }
