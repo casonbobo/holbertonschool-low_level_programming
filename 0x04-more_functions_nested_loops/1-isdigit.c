884aad16b14f2599d0600ac186c7f0f599a20e50
casonbobo <casonsbobo@gmail.com>
2023-02-02 09:01:40 -0800

    4th

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+/**
+*_isupper - Back at it
+*@c: c is a letter duh
+*Return: 1 or 0
+*/
+
+#include "main.h"
+
+int _isdigit(int c)
+{
+if (c >= 1 && c <= 9)
+	return (1);
+else
+	return (0);
+
+}
