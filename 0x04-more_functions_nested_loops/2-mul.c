b834a178230b9b645f8b5a06f4a830b7f606ccc7
casonbobo <casonsbobo@gmail.com>
2023-02-02 09:10:21 -0800

    no way its this easy

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/**
+*mul - makes the product 
+*@a: is a number secretly
+*@b wants to be a letter again
+*Retrun: 0
+*/
+
+int mul(int a, int b)
+{
+int product;
+
+a * b = product;
+
+return (0);
+}
