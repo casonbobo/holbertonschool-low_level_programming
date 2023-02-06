7d96b1a55f3857494833f2a1e315cf2d0cdca199
casonbobo <casonsbobo@gmail.com>
2023-02-06 10:17:34 -0800

    poiuy

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_strlen - Length of string
+ *@s = is a character
+ *Return: Does something
+ */
+
+int _strlen(char *s)
+{
+int i = *s;
+printf("%d\n", i);
+}
---
+++ 0x05-pointers_arrays_strings/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main - this is a main thing 
+ *Return: returns 0
+ */
+
+int main(void)
+{
+	    char *str;
+	        int len;
+
+		    str = "My first strlen!";
+		        len = _strlen(str);
+			    printf("%d\n", len);
+			        return (0);
+}
