53abb2232fe4c6529d6b4a4c12227be7007f1962
casonbobo <casonsbobo@gmail.com>
2023-02-06 09:04:16 -0800

    poiuyt

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ *reset_to_98 - counter counter
+ *@a: is a pointer
+ *@b: is a pointer too I guess
+ *Return: probably a nuthin
+ */
+#include "main.h"
+void swap_int(int *a, int b*)
+{
+ int y;
+ *a = y;
+ *b = *a;
+ y = *b;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,4 +3,5 @@
 #include <stdio.h>
 
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 #endif
