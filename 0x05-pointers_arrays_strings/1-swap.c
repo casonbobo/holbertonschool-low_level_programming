ff3ef74c4ec46db5900388d4ad6079d73899d2ea
casonbobo <casonsbobo@gmail.com>
2023-02-06 09:09:52 -0800

    poiuygf

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,10 +5,10 @@
  *Return: probably a nuthin
  */
 #include "main.h"
-void swap_int(int *a, int b*)
+void swap_int(int *a, int *b)
 {
- int y;
- *a = y;
- *b = *a;
- y = *b;
+int y;
+*a = y;
+*b = *a;
+y = *b;
 }
