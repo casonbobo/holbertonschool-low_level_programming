1a97c47e2fa034ac4d6e94b4a3c2d1dcce475bb9
casonbobo <casonsbobo@gmail.com>
2023-02-06 18:54:25 -0800

    [poiuy

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -16,7 +16,7 @@ void puts2(char *str)
 	}
 	for (i = 0; i < len; i += 2)
 	{
-		_putchar(str[i]);
+		putchar(str[i]);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,14 +17,14 @@ void puts_half(char *str)
 	{
 		for (i = len / 2; str[i] != '\0'; i++)
 		{
-			_putchar(str[i]);
+			putchar(str[i]);
 		}
 	}
 	else if (len % 2)
 	{
 		for (n = (len - 1) / 2; n < len - 1; n++)
 		{
-			_putchar(str[n + 1]);
+			putchar(str[n + 1]);
 		}
 	}
 	_putchar('\n');
