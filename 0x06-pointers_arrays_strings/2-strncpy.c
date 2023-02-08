2ffdc3ea55a6e68fc2ea0a1f0bd7c32c80dfc3d2
casonbobo <casonsbobo@gmail.com>
2023-02-08 14:52:01 -0800

    poiuy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,19 @@
 #include "holberton.h"
 #include <string.h>
 /**
- *_strncat - the straight cat
+ *_strncpy - the straight copy
  *@dest: is destination
  *@src: is something else
  *@n: is a number
  *Return: peepee poopoo
  */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int index = 0;
 
 	for (index = 0; src[index] && index < n; index++)
 		dest[index++] = src[index];
+	putchar('\0');
 	return (dest);
 }
