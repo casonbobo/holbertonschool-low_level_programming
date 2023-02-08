e926b6afc949e3b429cc1e598896c608d4441672
casonbobo <casonsbobo@gmail.com>
2023-02-08 10:25:00 -0800

    3rd

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <string.h>
 /**
  *_strcat - the straight cat
  *
@@ -7,9 +8,27 @@
 
 char *_strcat(char *dest, char *src)
 {
-char *array[100];
+int i = 0;
+int n = 0;
+int c = 0;
+int f = 0;
 
-strcpy(*array, src);
-strcat(*array, dest);
-return (*array);
+	while (dest[i] != 0)
+	{
+		i++;
+		n++;
+	}
+	while (src[c] != 0)
+	{
+		c++;
+		n++;
+	}
+	while (i <= n)
+	{
+		dest[i] = src[f];
+		i++;
+		f++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
