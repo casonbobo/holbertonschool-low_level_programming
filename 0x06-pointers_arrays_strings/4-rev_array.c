26dee8d9235eb0c4986170313c7febe6132bb8ad
casonbobo <casonsbobo@gmail.com>
2023-02-09 09:53:14 -0800

    poiuyt

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- * reverse_array - reverses an array
- * @a: aaaaaaaa
- * @n: nnnnnnnn
- * Return: nuthin
+ *reverse_array - reverses an array
+ *@a: aaaaaaaa
+ *@n: nnnnnnnn
+ *Return: nuthin
  */
 void reverse_array(int *a, int n);
 {
