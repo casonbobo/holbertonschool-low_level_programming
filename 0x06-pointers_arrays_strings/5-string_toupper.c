5d57a660d1321a7a85fea8267a5de931db35a362
casonbobo <casonsbobo@gmail.com>
2023-02-09 12:36:55 -0800

    poikmnh

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ *string_toupper - toooooo upper and beyond
+ *@str: spooder man
+ *Return: also stringyyy
+ */
+
+char *string_toupper(char *str)
+{
+	index = 0;
+
+	while (str[index] >= 'a' && str[index] <= 'z')
+		str[index] -= 32;
+	return(str);
+}
