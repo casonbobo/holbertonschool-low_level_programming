a4a65bdbc77100e988cdc418433c28b802f6559a
casonbobo <casonsbobo@gmail.com>
2023-02-09 08:40:04 -0800

    poiuy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,10 +18,12 @@ char *_strncpy(char *dest, char *src, int n)
 		index++;
 		srclen++;
 	}
-	for (; src[index] && index < n ; index++)
+
+	for (index = 0; src[index] && index < n ; index++)
 	{
 		dest[index] = src[index];
 	}
+
 	for (index = srclen; index < n; index++)
 	{
 		dest[index] = '\0';
