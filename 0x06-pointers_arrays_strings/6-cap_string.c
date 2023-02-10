1f3eada6dabe670d5902b9627266374003e2e810
casonbobo <casonsbobo@gmail.com>
2023-02-10 09:39:27 -0800

    poiuytr

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,29 +6,29 @@
 
 char *cap_string(char *str)
 {
-int index = 0;
+	int index = 0;
 
-while (str[index])
-{
-	while (!(str[index] >= 'a' && str[index] <= 'z'))
+	while (str[index])
 	{
-		index++;
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+		{
+			index++;
 		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}' ||
-				index == 0)
-				str[index] -=32;
-				index++;
+		str[index - 1] == '\t' ||
+		str[index - 1] == '\n' ||
+		str[index - 1] == ',' ||
+		str[index - 1] == ';' ||
+		str[index - 1] == '.' ||
+		str[index - 1] == '!' ||
+		str[index - 1] == '?' ||
+		str[index - 1] == '"' ||
+		str[index - 1] == '(' ||
+		str[index - 1] == ')' ||
+		str[index - 1] == '{' ||
+		str[index - 1] == '}' ||
+		index == 0)
+			str[index] -= 32;
+			index++;
 	}
 }
 return (str);
