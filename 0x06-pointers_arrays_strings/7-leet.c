efb058312086793ff4396a568f1882ecdd77e741
casonbobo <casonsbobo@gmail.com>
2023-02-10 11:28:50 -0800

    poiuy

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,17 +8,17 @@ char *leet(char *str)
 {
 	char a[5] = {'A', 'E', 'O', 'T', 'L'};
 	char b[5] = {'4', '3', '0', '7', '1'};
-	int count1 = 0, count2 = 0, count3 = 0;
+	int count1 = 0, count2 = 0;
 
-	while(str[count1])
+	while (str[count1])
 	{
 		count2 = 0;
 
-		while(count2 < 1)
+		while (count2 < 5)
 		{
 			if (str[count1] == a[count2] || str[count1] - 32 == a[count2])
 			{
-				str[count1] = b[count2]
+				str[count1] = b[count2];
 			}
 
 			count2++;
