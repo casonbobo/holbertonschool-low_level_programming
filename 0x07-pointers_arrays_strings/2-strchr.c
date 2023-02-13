4699ec8145548c2a24b1baf99e63bd38917012a0
casonbobo <casonsbobo@gmail.com>
2023-02-13 10:26:09 -0800

    pee pee poo poo

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_strchr - straight characters are overrated
+ *@s: ssssss (like a sausage)
+ *@c: cccccc (like a cold)
+ *Return: again why is this captilized??
+ */
+
+char *_strchr(char *s, char c)
+{
+	int count = 0;
+
+	for (; s[count]; count++)
+	{
+		if (s[count] == c)
+			return (s);
+	}
+
+	return ('\0');
+}
