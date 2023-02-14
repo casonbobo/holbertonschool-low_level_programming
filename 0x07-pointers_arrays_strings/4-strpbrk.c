697f6bdd44f6832d376fcf7a798244165e60999c
casonbobo <casonsbobo@gmail.com>
2023-02-13 18:31:22 -0800

    poiuy

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_strpbrk -  stop the barking
+ *@s: ssssss (like a squirrel?)
+ *@accept: please accept this apology
+ *Return: Returns a chaarrrrrr
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (s[i] == accept[n])
+			{
+			return (s + i);
+			}
+		}
+	}
+return (0)
+}
