603a605cc88a5de30b6e541d3d0e988927485040
casonbobo <casonsbobo@gmail.com>
2023-02-15 09:08:02 -0800

    first of the day

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_puts_recursion - recurse
+ *@s: ssssss (ssssss)
+ *Return;
+ */
+
+void _puts_recursion(char *s)
+{
+	int count = 0;
+
+	_putchar(s[count]);
+	count++;
+}
