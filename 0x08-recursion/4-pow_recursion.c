0b068c8b3dea6ac1e9317fa0db17cc4caa01b5e0
casonbobo <casonsbobo@gmail.com>
2023-02-15 14:09:04 -0800

    how bout now

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,11 +11,8 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	else if ( y == 0 || 1 )
-		return (x);
+	else if (y == 0)
+		return (1);
 	else
-	{
-		y--;
-		return (x * x);
-	}
+		return (x * _pow_recursion(x, y - 1));
 }
