6a2e1038772d23ed21dd125c76101c7125f3ff2a
casonbobo <casonsbobo@gmail.com>
2023-02-15 12:43:43 -0800

    poiuyt

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,12 +3,12 @@
 /**
  *_strlen_recursion - string length the long way
  *@s: ssssss ( like somethinggggg)
- *Return;
+ *Return: return something
  */
 
 int _strlen_recursion(char *s)
 {
-if (s != '\0')
+if (*s != '\0')
 return (1 + _strlen_recursion(s + 1));
 else
 return (0);
