057e8856f4af60f522f7bb83656048e87fad1e61
casonbobo <casonsbobo@gmail.com>
2023-02-16 08:46:23 -0800

    maybe huh

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_sqrt_recursion - watch it there squirt
+ *@n: night night
+ *Return: return what though
+ */
+
+int _sqrt_recursion(int n)
+{
+	int c = 0;
+
+	if (n != '\0')
+		if (c * c != n)
+			_sqrt_recursion(c++);
+		else
+			return (c);
+	else
+		return (-1);
+}
