0c5b5c45c44adec41714fc0409d0b84fabedab46
casonbobo <casonsbobo@gmail.com>
2023-02-22 11:48:16 -0800

    poiuyt

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,13 @@
 #ifndef _HEADER_
 #define _HEADER_
-#define  ABS( |x| );
-#endif
+#define  ABS(x)
+/**
+ *ABS - absolute babe
+ *@x: xxxxxx (like xinger)
+ *Return: x ing
+ */
+
+int ABS(int x)
+{
+	return (x = x % 1);
+}
