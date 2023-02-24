d4924be84afef78a8a85985e60a5c2be7af10dcc
casonbobo <casonsbobo@gmail.com>
2023-02-24 10:58:17 -0800

    try this on for size

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,28 +5,24 @@
  *@s1: stringyy
  *@s2:strungyy
  *@n: nice and neat
- * Return: my character
+ *Return: my character
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i1, i2, malc;
+	unsigned int index1, index2;
 	char *o;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	for (i1 = 0; s1[i1] != '\0'; i1++)
-		s1[i1] = o[i1];
-	i1++;
-	for (i2 = 0; i2 != n || i2 != '\0'; i2++, i1++)
-		s2[i2] = o[i1];
-	for (malc = 0; malc < i1; malc++)
+	for (index1 = 0; s1[index1]; index1++)
 	{
 		o = malloc(sizeof(char));
 		if (o == NULL)
 			return (NULL);
 	}
+	for (index1 = 0; s1[index1]; index1++)
+		o[index1] = s1[index1];
+	for (index2 = 0; s2[index2] && index2 <= n; index2++, index1++)
+		o[index1] = s2[index2];
+
 	return (o);
 }
