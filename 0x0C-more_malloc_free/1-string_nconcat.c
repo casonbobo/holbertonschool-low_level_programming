5a12734137aa3fbc0a500b3a44ee26ae8aa61e4d
casonbobo <casonsbobo@gmail.com>
2023-02-24 11:13:13 -0800

    poiuyt

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,19 +10,22 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int index1, index2;
-	char *o;
-
-	for (index1 = 0; s1[index1]; index1++)
-	{
-		o = malloc(sizeof(char));
-		if (o == NULL)
-			return (NULL);
-	}
-	for (index1 = 0; s1[index1]; index1++)
-		o[index1] = s1[index1];
-	for (index2 = 0; s2[index2] && index2 <= n; index2++, index1++)
-		o[index1] = s2[index2];
-	o[index1] = '\0';
-	return (o);
+	char *concat;
+	unsigned int len = n, index;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (index = 0; s1[index]; index++)
+		len++;
+	concat = malloc(sizeof(char) * (len + 1));
+	if (concat == NULL)
+		return (NULL);
+	len = 0;
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+	concat[len] = '\0';
+	return (concat);
 }
