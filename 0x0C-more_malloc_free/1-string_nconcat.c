726a3c54fd4aa8bfd15d816e6033a3cf5e9d129d
casonbobo <casonsbobo@gmail.com>
2023-02-27 12:14:46 -0800

    does this work

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,33 +10,39 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	unsigned int len = n, index;
+
+	unsigned int s1Len = 0, s2Len = 0, bufferLen, index = 0;
+	char *pointer;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[index])
-		index++;
-	while (s2[len++])
-		index++;
+	while (s1[s1Len])
+		s1Len++;
+	while (s2[s2Len])
+		s2Len++;
+	if (s2Len > n)
+		s2Len = n;
 
-	concat = malloc(sizeof(char) * (index + 1));
+	bufferLen = s1Len + s2Len;
 
-	if (concat == NULL)
+	pointer = malloc(sizeof(char) * (bufferLen + 1));
+	if (pointer == NULL)
+
 		return (NULL);
-
-	while (s1[index])
+	s1Len = 0;
+	for (index = 0; s1[index]; index++)
 	{
-		s1[index] = concat[index];
-		index++;
+		pointer[s1Len] = s1[index];
+		s1Len++;
 	}
-	while (s2[len])
+	for (index = 0; s2[index] && index < bufferLen; index++)
 	{
-		s2[len] = concat[index];
-		index++;
+		pointer[s1Len] = s2[index];
+		s1Len++;
+		s2Len++;
 	}
-	concat[index] = '\0';
-	return (concat);
+	pointer[s1Len] = '\0';
+	return (pointer);
 }
