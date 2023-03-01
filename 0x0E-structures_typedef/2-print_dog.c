75af7c43e8e368f32fdd0761764588bd301615ae
casonbobo <casonsbobo@gmail.com>
2023-03-01 12:14:06 -0800

    poiuyt

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,7 +7,10 @@
 
 void print_dog(struct dog *d)
 {
+	if (d)
+	{
 	printf("Name: %s\n", (*d).name ? (*d).name : "(nil)");
 	printf("Age: %f\n", (*d).age ? (*d).age : "(nil)");
 	print("Owner: %s\n", (*d).owner ? (*d).owner : "(nil)");
+	}
 }
