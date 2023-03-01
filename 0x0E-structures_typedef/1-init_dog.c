b0a996a4600b54c9f1c6d9804a8ed393a5a2f352
casonbobo <casonsbobo@gmail.com>
2023-03-01 11:06:27 -0800

    oiuyt

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,5 +11,9 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
-		struct dog my_dog = {"Poppy", 3.5, "Bob"};
+	{
+		d.name = name;
+		d.age = age;
+		d.owner = owner;
+	};
 }
