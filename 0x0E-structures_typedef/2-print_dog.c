7bc19a76cfd3d06549e69a2c17f7f063931e8cb6
casonbobo <casonsbobo@gmail.com>
2023-03-01 12:12:48 -0800

    what happens

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,7 +7,7 @@
 
 void print_dog(struct dog *d)
 {
-	printf("Name: %d\n", (*d).name ? (*d).name : "(nil)");
+	printf("Name: %s\n", (*d).name ? (*d).name : "(nil)");
 	printf("Age: %f\n", (*d).age ? (*d).age : "(nil)");
-	print("Owner: %d\n", (*d).owner ? (*d).owner : "(nil)");
+	print("Owner: %s\n", (*d).owner ? (*d).owner : "(nil)");
 }
