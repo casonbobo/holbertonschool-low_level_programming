8e3f1fb0cffbea2e60fea1e50b39d96fa08bec04
casonbobo <casonsbobo@gmail.com>
2023-03-01 11:00:54 -0800

    drive my own yacht

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ *init_dog - pee pee oiioii
+ *@d: heres my dog
+ *@name: my dogs name
+ *@age: my dogs old
+ *@owner: i pick up the poop
+ *Return: 0
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+		struct dog my_dog = {"Poppy", 3.5, "Bob"};
+}
