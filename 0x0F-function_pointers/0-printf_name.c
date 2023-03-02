c0f0d89b9366de476b33e7163e6db6b67f37b7cb
casonbobo <casonsbobo@gmail.com>
2023-03-02 11:47:14 -0800

    poiuy

---
+++ 0x0F-function_pointers/0-printf_name.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_name - print my name
+ *@name: my name is
+ *@f: fffffff (flip flop)
+ *Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	printf("Hello, my name is %s\n", name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This project is going to suck massive turds
