58a0c9375b6276a23882d4f7b284e573d4154444
casonbobo <casonsbobo@gmail.com>
2023-03-03 13:53:29 -0800

    poiuyt

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,10 +10,13 @@ int main(int argc, char *argv[])
 {
 	int a, b;
 	int (*operator)(int, int);
+	
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
 	if (argc != 4)
 	{
-		printf("Error98\n");
+		printf("Error\n");
 		exit(98);
 	}
 
@@ -25,14 +28,11 @@ int main(int argc, char *argv[])
 
 	operator = get_op_func(argv[2]);
 
-	if ((argv[2][1] == '/' || '%') && (argv[3][1] == '0'))
+	if ((*argv[2] == '/' || '%') && (b == 0))
 	{
-		printf("Error100\n");
+		printf("Error\n");
 		exit(100);
 	}
-
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
 
 	printf("%d\n", operator(a, b));
 	return (0);
