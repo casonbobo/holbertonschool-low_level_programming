983ef17416b94df77aaba6f0193282e741f8f7f6
casonbobo <casonsbobo@gmail.com>
2023-03-03 11:18:16 -0800

    1st push for this dumb project

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+/**
+ *op_add - Add
+ *@a: first number
+ *@b: second number
+ *Return: Add
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *op_sub - Sub
+ *@a: first number
+ *@b: second number
+ *Return: Sub
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *op_mul - Mul
+ *@a: first number
+ *@b: second number
+ *Return: Mul
+ */
+
+int op_mul(int a. int b)
+{
+	return (a * b);
+}
+/**
+ *op_div - Div
+ *@a: first number
+ *@b: second number
+ *Return: Div
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *op_mod - Mod
+ *@a: first number
+ *@b: second number
+ *Return: Mod
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
