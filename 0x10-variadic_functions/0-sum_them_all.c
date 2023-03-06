2e4364d0dfefac4ac36d047a840b87f5333604aa
casonbobo <casonsbobo@gmail.com>
2023-03-06 08:48:42 -0800

    maybe this will work

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,6 +3,25 @@
 /**
  *sum_them_all - kill them all
  *@n: county county
- *
- *
- *
+ *Return: I'm really sorry Evan
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	int sum = 0;
+	unsigned int i;
+	
+	if (n == 0)
+		return (0);
+
+	va_start(list, n)
+	while (i < n)
+	{
+		sum = sum + va_arg(list, int);
+		i++;
+	}
+	va_end(list);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+I don't know what to put here. Im just living in this plain of existence
