f149f51751acee3c530012ba5fefdf8afc78fa4c
casonbobo <casonsbobo@gmail.com>
2023-03-06 13:44:31 -0800

    the peepee poopoo man is coming

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+/**
+ *print_strings - a loom prints strings
+ *@separator: separates
+ *@n: i do declare
+ *Return: nothing im quite sure
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list list;
+	char str;
+
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	va_start(list, n);
+	while (i < n - 1)
+	{
+		str = va_arg(list, char *);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+		if (separator != NULL)
+			printf("%s", separator);
+		i++;
+	}
+	str = va_arg(list, char *);
+	if (str == NULL)
+		printf("(nil)\n");
+	else
+		printf("%s\n", str);
+}
