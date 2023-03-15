27da649275e2c2b72d6da6812960322da762d302
casonbobo <casonsbobo@gmail.com>
2023-03-15 13:59:14 -0700

    ppoiuyt

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *free_list - free what?
+ *@head: the head of our list
+ *Return: no return
+ */
+
+void free_list(list_t *head)
+{
+	list_t temp;
+	
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+	free(head);
+}
