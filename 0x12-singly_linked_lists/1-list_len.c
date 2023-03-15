af23e48a89e7c80f7ff074942981d37de264d00e
casonbobo <casonsbobo@gmail.com>
2023-03-15 10:55:25 -0700

    oiuyt

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ *list_len - counts the length of a list
+ *@h: is an h
+ *Return: a count?
+ */
+
+size_t list_len(const list_t *h)
+{
+int count = 0;
+const list_t car = h;
+while (car)
+	if (car != NULL)
+		count++;
+	return (count);
+}
