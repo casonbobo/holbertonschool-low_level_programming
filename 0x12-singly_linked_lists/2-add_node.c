de27688554d6af19d000c2122491d2ee92a0ebdc
casonbobo <casonsbobo@gmail.com>
2023-03-15 11:57:58 -0700

    this doesn't make any sense

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ *add_node - adds a node. I think
+ *@head: so no head
+ *@str: a string duhhhh
+ *Return: an address or null
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode;
+
+	newNode = malloc(sizeof(list_t));
+
+	if (!newNode)
+	{
+		free(newNode);
+		return (NULL);
+	}
+
+	newNode->next = *head
+	(*head) = newNode;
+	(*head)->str = strdup(str);
+	(*head)->len = strlen(str);
+	(*head)->next = newNode->next;
+}
