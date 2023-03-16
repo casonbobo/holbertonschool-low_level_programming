3af3c78eb73c1be2dd2ee44f32547d6cff645d87
casonbobo <casonsbobo@gmail.com>
2023-03-16 13:05:10 -0700

    i dont even know anymore

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ *add_nodeint - adding notes and numbers
+ *@head: header
+ *@n: is an n
+ *Return: the address or the NULL
+ */
+
+listint_t *add_nodeint(listint_t **head, consst int n)
+{
+	list_t *newNode = malloc(sizeof(list_t));
+
+	if (!newNode)
+	{
+		free(newNode);
+		return (NULL);
+	}
+
+	newNode->next = *head;
+	(*head)->n =  strdup(n);
+	(*head)->len = strlen(n);
+	(*head)->next = newNode->next;
+
+	return (*head);
+}
