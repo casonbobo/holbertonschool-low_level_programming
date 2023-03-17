3e48c4baf7eb65fc06fc57e367143eeba4a43031
casonbobo <casonsbobo@gmail.com>
2023-03-17 09:02:34 -0700

    back at it

---
+++ 0x13-more_singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ *add_nodeint_end - this adds
+ *@head: if the head causes you to sin cut it off
+ *@n: is a letter and likes it that way
+ *Return: newNode
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *newNode = malloc(sizeof(listint_t));
+	listint_t *tempCycle = *head;
+
+	if (!newNode)
+	{
+		free(newNode);
+		return (NULL);
+	}
+	newNode->n = n;
+	newNode->next = tempCycle;
+	*head = newNode;
+
+	if (*head == NULL)
+	{
+		newNode = *head;
+		return (newNode);
+	}
+
+	while (tempCycle->next)
+		tempCycle = tempCycle->next;
+	tempCycle->next = newNode;
+	return (newNode);
+}
