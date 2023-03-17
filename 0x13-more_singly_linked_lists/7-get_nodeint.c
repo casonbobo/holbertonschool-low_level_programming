9ed5bf494130a1e84a966198aab3fa6b93aaaa93
casonbobo <casonsbobo@gmail.com>
2023-03-17 11:54:08 -0700

    they made me cut down a joke cause it was too long :(

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ *get_nodeint_at_index - count to place and then give me the thing at place
+ *@head: hows your head
+ *@index: im mr. numbers i do the counting around here
+ *Return: return to where you came
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *temp = head;
+	unsigned int count;
+
+	while (count < index)
+	{
+		if (count == index)
+			return (temp);
+		temp = temp->next;
+		count++;
+	}
+	return (NULL);
+}
