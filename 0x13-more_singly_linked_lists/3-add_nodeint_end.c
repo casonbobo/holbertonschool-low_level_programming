edd27fc5c62a2197d6f588dd7f9316ea076d5aeb
casonbobo <casonsbobo@gmail.com>
2023-03-17 09:22:51 -0700

    ILL POOP MY PANTS

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,7 +8,7 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *newNode = malloc(sizeof(listint_t));
+	listint_t *newNode = malloc(sizeof(*newNode));
 	listint_t *tempCycle = *head;
 
 	if (!newNode)
