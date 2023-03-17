a7085fe49cda39d0e5157054f5b058fb2d127866
casonbobo <casonsbobo@gmail.com>
2023-03-17 09:20:18 -0700

    LELELELELELELELEL

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -17,8 +17,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (NULL);
 	}
 	newNode->n = n;
-	newNode->next = tempCycle;
-	*head = newNode;
+	newNode->next = NULL;
 
 	if (*head == NULL)
 	{
