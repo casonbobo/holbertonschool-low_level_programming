387e125bca6c2001dfe8bc0cc8287463fc00ea00
casonbobo <casonsbobo@gmail.com>
2023-03-20 13:52:00 -0700

    plz now mr mathew

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,17 +7,17 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file;
+	int fd;
 
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (file == -1)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
 
 	if (text_content)
 	{
-		if (write(file, text_content, strlen(text_content)) == -1)
-			return (-1)
+		if (write(fd, text_content, strlen(text_content)) == -1)
+			return (-1);
 	}
-	close(file);
+	close(fd);
 	return (1);
 }
