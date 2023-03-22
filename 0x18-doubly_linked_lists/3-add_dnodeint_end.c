8cb2834f8ee4f539c5daf34f8d0e425235b23d1d
casonbobo <casonsbobo@gmail.com>
2023-03-22 10:59:14 -0700

    adels here

--- 0x18-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x18-doubly_linked_lists/3-add_dnodeint_end.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	newNode->n = n;
 	newNode->next = NULL;
-	newNode->prev = NULL
+	newNode->prev = NULL;
 	if (*head == NULL)
 	{
 		*head = newNode;
