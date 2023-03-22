fbe1c2b3f348e6aec6c587df11277244e8e70b3e
casonbobo <casonsbobo@gmail.com>
2023-03-22 09:47:16 -0700

    caramon is smart :)

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -10,7 +10,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newNode;
 
-	newNode = malloc(sizeof(list_t));
+	newNode = malloc(sizeof(dlistint_t));
 
 	if (!newNode)
 	{
@@ -18,11 +18,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 
+	(*head)->n = n;
 	newNode->next = *head;
-	(*head) = newNode;
-	(*head)->str = strdup(str);
-	(*head)->len = strlen(str);
-	(*head)->next = newNode->next;
 	newNode->prev = NULL;
 
 	return (*head);
