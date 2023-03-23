cfac4212ac6f8413886698c5cddf7760e2fc61bc
casonbobo <casonsbobo@gmail.com>
2023-03-23 09:47:04 -0700

    fixed a bunch of dumb spaces. Sarahs still mean

--- 0x18-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x18-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,28 +8,24 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *temp = *head;
-    unsigned int i;
+	dlistint_t *temp = *head;
+	unsigned int i;
 
-    if (*head == NULL)
-        return (-1);
+	if (*head == NULL)
+		return (-1);
 
-    for (i = 0; temp != NULL && i < index; i++)
-        temp = temp->next;
+	for (i = 0; temp != NULL && i < index; i++)
+		temp = temp->next;
 
-    if (temp == NULL)
-        return (-1);
+	if (temp == NULL)
+		return (-1);
 
-    if (*head == temp)
-        *head = temp->next;
-
-    if (temp->next != NULL)
-        temp->next->prev = temp->prev;
-
-    if (temp->prev != NULL)
-        temp->prev->next = temp->next;
-
-    free(temp);
-
-    return (1);
+	if (*head == temp)
+		*head = temp->next;
+	if (temp->next != NULL)
+		temp->next->prev = temp->prev;
+	if (temp->prev != NULL)
+		temp->prev->next = temp->next;
+	free(temp);
+	return (1);
 }
