d3df009da9724153aef281ff3850d5e8e9352502
casonbobo <casonsbobo@gmail.com>
2023-03-24 10:09:12 -0700

    poiuy

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!hash_table || !size)
 		return (NULL);
 
-	hash_table->array = malloc(sizeof(hash_node_t) * size);
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (!hash_table->array)
 		return (NULL);
