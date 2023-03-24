01caa8b09aeaa695b6b0b326725d88c59e7c619c
casonbobo <casonsbobo@gmail.com>
2023-03-24 10:00:12 -0700

    poiuy

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,10 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table->array = malloc(sizeof(hash_node_t) * size);
 
 	if (!hash_table->array)
-	{
-		hash_table = NULL;
 		return (NULL);
-	}
 
 	while (index++ < size)
 		hash_table->array[index] = NULL;
