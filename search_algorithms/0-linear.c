75df72895ba1a88f4e83930de66a8a3fe8a41ebe
casonbobo <118225257+casonbobo@users.noreply.github.com>
2023-12-13 14:15:42 -0600

    betty no like space indents

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -14,13 +14,13 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
+	size_t i;
 
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-    }
-    return (-1);
-}
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+		return (-1);
+	}
