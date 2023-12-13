0820a479664ad5df3ae1bbc0177542c813e132bb
casonbobo <118225257+casonbobo@users.noreply.github.com>
2023-12-13 16:26:06 -0600

    Will this work? moved some logic around

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -16,27 +16,26 @@ int binary_search(int *array, size_t size, int value)
 {
 	int l = 0;
 	int r = size - 1;
-	int i = 0;
-
-	printf("Searching in array: ");
-
-	for (i = l; i < r; i++)
-		printf("%d, ", array[i]);
-
-	printf("%d", array[i]);
-	printf("\n");
-
+	
 	while (l <= r)
 	{
+		printf("Searching in array: ");
+
+		int i = 0;
+		for (i = l; i < r; i++)
+			printf("%d, ", array[i]);
+		
+		printf("%d", array[i]);
+		printf("\n");
 		int mid = l + (r - 1) / 2;
 
 		if (array[mid] == value)
-			return mid;
+			return (mid);
 
 		if (array[mid] < value)
 			l = mid + 1;
 		else
 			r = mid - 1;
 	}
-	return -1;
+	return (-1);
 }
